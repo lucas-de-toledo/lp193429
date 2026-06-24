65f9519f6c01c533f9217c1751b189a27e0ab57d
lucas-de-toledo <lucas.toledo@unifesp.br>
2026-06-23 22:34:39 -0300

    Update 1383.c

--- 1383.c
+++ 1383.c
@@ -13,9 +13,10 @@ Pergunta de segunda ordem: -
 
 int verifica_grupo(int freq[]){
     
+    //Percorre os valores armazenados nas posições de 1 a 9. No código, essas posições correspondem aos próprios números verificados
     for(int i = 1; i <= 9; i++){
         
-        if(freq[i] != 1) return 0;
+        if(freq[i] != 1) return 0; //Cada um pode "aparecer" uma única vez
     }
     
     return 1;
@@ -30,14 +31,18 @@ int main(){
         
         int sudoku[9][9];
         
+        int valido = 1;
+        
+        //Preencher matriz e ver se numeros são válidos
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 
                 scanf("%d", &sudoku[i][j]);
+                
+                //Garante números entre 1 e 9
+                if(sudoku[i][j] < 1 || sudoku[i][j] > 9) valido = 0;
             }
         }
-        
-        int valido = 1;
         
         
         for(int i = 0; i < 9 && valido; i++){
@@ -46,18 +51,10 @@ int main(){
             
             for(int j = 0; j < 9; j++){
                 
-                int num = sudoku[i][j];
-                
-                if(num < 1 || num > 9){
-                    
-                    valido = 0;
-                    break;
-                }
-                
-                freq[num]++;
+                freq[sudoku[i][j]]++; //Soma uma ocorrência para cada número de 1 a 9, a cada linha
             }
             
-            if(!verifica_grupo(freq)) valido = 0;
+            if(!verifica_grupo(freq)) valido = 0; //A cada linha, verifica se é válida
         }
         
         
@@ -67,32 +64,33 @@ int main(){
             
             for(int i = 0; i < 9; i++){
                 
-                freq[sudoku[i][j]]++;
+                freq[sudoku[i][j]]++; //Soma uma ocorrência para cada número de 1 a 9, a cada coluna
             }
             
-            if (!verifica_grupo(freq)) valido = 0;
+            if(!verifica_grupo(freq)) valido = 0; //A cada coluna, verifica se é válida
         }
         
-        
+        //Inicia no canto superior esquerdo de cada bloco 3x3
         for(int lin = 0; lin < 9 && valido; lin += 3){
             for(int col = 0; col < 9 && valido; col += 3){
                 
                 int freq[10] = {0};
                 
+                //Considera 3 posições, a frente e abaixo, a partir dos cantos
                 for(int i = lin; i < lin + 3; i++){
                     for(int j = col; j < col + 3; j++){
                         
-                        freq[sudoku[i][j]]++;
+                        freq[sudoku[i][j]]++; //Soma uma ocorrência para cada número de 1 a 9, a cada bloco
                     }
                 }
                 
-                if(!verifica_grupo(freq)) valido = 0;
+                if(!verifica_grupo(freq)) valido = 0; //A cada bloco, verifica se é válido
             }
         }
         
         printf("Instancia %d\n", instancia);
         
-        if (valido) printf("SIM\n\n");
+        if(valido) printf("SIM\n\n");
         
         else printf("NAO\n\n");
     }
